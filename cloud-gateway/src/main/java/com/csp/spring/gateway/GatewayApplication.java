54209c75d7faf433f36cb8a17e99f04def10e3e9
chensiping <chensiping@tetras.ai>
2022-09-16 20:53:37 +0800

    add cloud-gateway

---
+++ cloud-gateway/src/main/java/com/csp/spring/gateway/GatewayApplication.java
@@ -0,0 +1,20 @@
+package com.csp.spring.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author chensiping
+ * @since 2022-09-16
+ */
+@EnableDiscoveryClient
+@EnableFeignClients
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
