d64df59dd547fc9c240b6b768d499144c12f8793
chensiping <chensiping@tetras.ai>
2022-09-16 19:39:09 +0800

    upgrade jdk/springboot/springcloud version

---
+++ cloud-eureka/src/main/java/com/csp/spring/eureka/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.csp.spring.eureka;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author chensiping
+ * @since 2022-09-16
+ */
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    }
+
+}
