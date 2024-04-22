88e927a2872f6eb86f346e26f3281ff90918867e
chensiping <chensiping@tetras.ai>
2024-04-22 15:18:19 +0800

    fix tenant

--- micro-user/src/main/java/com/csp/cloud/user/controller/TouristController.java
+++ micro-user/src/main/java/com/csp/cloud/user/controller/TouristController.java
@@ -1,14 +1,11 @@
 package com.csp.cloud.user.controller;
 
-
+import com.csp.cloud.infra.core.exception.NotFoundException;
 import com.csp.cloud.infra.web.aspects.UserContextHolder;
 import com.csp.cloud.user.model.entity.Tourist;
 import com.csp.cloud.user.service.TouristService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -20,19 +17,25 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2024-04-19
  */
 @RestController
-@RequestMapping("/v1/tourist")
+@RequestMapping("/v1/tourists")
 public class TouristController {
 
-    @Autowired
-    private UserContextHolder userContextHolder;
+    private final UserContextHolder userContextHolder;
 
-    @Autowired
-    private TouristService touristService;
+    private final TouristService touristService;
+
+    public TouristController(UserContextHolder userContextHolder, TouristService touristService) {
+        this.userContextHolder = userContextHolder;
+        this.touristService = touristService;
+    }
 
     @GetMapping("/me")
     public Object me() {
         String userId = userContextHolder.getUserId();
         Tourist tourist = touristService.lambdaQuery().eq(Tourist::getUserId, userId).one();
+        if (tourist == null) {
+            throw new NotFoundException("Tourist not exist");
+        }
         return tourist;
     }
 }
